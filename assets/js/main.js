c588aca1d8672ae019614b31c688d50e3c60d5f9
SupCtrlVol <supervisor.volumetricos@wigas.com.mx>
2026-06-17 14:59:49 -0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,23 +58,22 @@ function toggleMenu() {
   menu.classList.toggle('open');
 }
 
-// Bloque modular del banner
+// ====== Banner superior ======
 function renderBanner() {
   return `
     <div class="banner-superior">
       <div class="banner-blanco">
-        <div class="banner-logo" onclick="mostrarBienvenida()">
-          <img src="assets/images/logo_blanco.webp?v=5" alt="Logo">
+        <div class="banner-logo">
+          <img src="assets/images/logo.png" alt="Logo" onclick="mostrarBienvenida()">
         </div>
       </div>
       <div class="banner-rojo">
         <div class="banner-menu">
-          <button onclick="toggleMenu()">Menú ▾</button>
+          <button onclick="toggleMenu()">Menú</button>
           <div class="banner-menu-opciones">
-            <a href="#" onclick="abrirVista('balance')">Balance de Energía</a>
-            <a href="#" onclick="abrirVista('herramientas')">Herramientas</a>
-            <a href="#" onclick="abrirVista('ventas')">Ventas</a>
-            <a href="#" onclick="abrirVista('cumplimiento')">Cumplimiento</a>
+            <a href="#" onclick="mostrarCategorias()">Categorías</a>
+            <a href="#" onclick="mostrarAcuses('13 GAS')">Acuses</a>
+            <a href="#" onclick="mostrarReporte('wigas_be')">Reportes</a>
           </div>
         </div>
       </div>
@@ -82,67 +81,81 @@ function renderBanner() {
   `;
 }
 
-// Vista ejecutiva con recuadros y banner (clase banner-view)
-function abrirVista(categoria) {
-  let contenido = renderBanner();
+function toggleMenu() {
+  document.querySelector('.banner-menu').classList.toggle('open');
+}
 
-  if(categoria === 'balance') {
-    contenido += `
-      <div class="card p-3 bienvenida banner-view">
-        <h2>Balance de Energía</h2>
-        <p>Seleccione la estación:</p>
-        <div class="opciones-vista">
-          <div class="opcion-recuadro" onclick="mostrarReporte('13gas_be')">13 GAS</div>
-          <div class="opcion-recuadro" onclick="mostrarReporte('bexica_be')">BEXICA</div>
-          <div class="opcion-recuadro" onclick="mostrarReporte('coacalco_be')">
-          <span class="texto multilinea">CONSORCIO DE GNV<br>Coacalco</span></div>
-          <div class="opcion-recuadro" onclick="mostrarReporte('ecatepec_be')">
-          <span class="texto multilinea">CONSORCIO DE GNV<br>Ecatepec</span></div>
-          <div class="opcion-recuadro" onclick="mostrarReporte('tlanepantla_be')">
-          <span class="texto multilinea">CONSORCIO DE GNV<br>Tlanepantla</span></div>
-          <div class="opcion-recuadro" onclick="mostrarReporte('landsegen_be')">LANDSEGEN</div>
-          <div class="opcion-recuadro" onclick="mostrarReporte('wigas_be')">WIGAS</div>
+// ====== Pantalla de bienvenida ======
+function mostrarBienvenida() {
+  let contenido = `
+    <div class="card p-3 bienvenida inicio-view">
+      <h2>Bienvenido al Portal CCI Wigas</h2>
+      <p>Seleccione una opción para continuar:</p>
+      <div class="opciones-principales">
+        <div class="opcion-circulo" onclick="mostrarCategorias()">
+          <img src="assets/images/categorias.png" class="icono-img">
+          <span>Categorías</span>
         </div>
-      </div>`;
-  } else if(categoria === 'herramientas') {
-    contenido += `
-      <div class="card p-3 bienvenida banner-view">
-        <h2>Herramientas de Análisis</h2>
-        <p>Seleccione la estación:</p>
-        <div class="opciones-vista">
-          <div class="opcion-recuadro" onclick="mostrarReporte('13gas_tool')">13 GAS</div>
-          <div class="opcion-recuadro" onclick="mostrarReporte('wigas_tool')">WIGAS</div>
+        <div class="opcion-circulo" onclick="mostrarAcuses('13 GAS')">
+          <img src="assets/images/acuses.png" class="icono-img">
+          <span>Acuses</span>
         </div>
-      </div>`;
-  } else if(categoria === 'ventas') {
-    contenido += `
-      <div class="card p-3 bienvenida banner-view">
-        <h2>Ventas</h2>
-        <p>Seleccione el reporte:</p>
-        <div class="opciones-vista">
-          <div class="opcion-recuadro" onclick="mostrarReporte('13gas_vd')">13 GAS</div>
-          <div class="opcion-recuadro" onclick="mostrarReporte('landsegen_vd')">LANDSEGEN</div>
-          <div class="opcion-recuadro" onclick="mostrarReporte('wigas_vd')">WIGAS</div>
-          <div class="opcion-recuadro" onclick="mostrarReporte('ventxcliente_trimes')">
-          <span class="texto multilinea">GLOBAL<br>Ventas por cliente</span></div>
+        <div class="opcion-circulo" onclick="mostrarReporte('wigas_be')">
+          <img src="assets/images/reportes.png" class="icono-img">
+          <span>Reportes</span>
         </div>
-      </div>`;
-} else if(categoria === 'cumplimiento') {
-    contenido += `
-      <div class="card p-3 bienvenida banner-view">
-        <h2>Cumplimiento</h2>
-        <p>Seleccione el reporte:</p>
-        <div class="opciones-vista">
-          <div class="opcion-recuadro" onclick="mostrarReporte('json_tool')">JSON Mensual</div>
-          <div class="opcion-recuadro" onclick="mostrarAcuses('13 GAS')">Acuses</div>
-        </div>
-      </div>`;
-  }
-
+      </div>
+    </div>
+  `;
   document.getElementById('contenedor').innerHTML = contenido;
 }
 
-// Mostrar acuses desde metadata.json
+// ====== Mostrar categorías ======
+function mostrarCategorias() {
+  let contenido = renderBanner();
+  contenido += `
+    <div class="card p-3 bienvenida banner-view">
+      <h2>Categorías</h2>
+      <p>Seleccione una estación:</p>
+      <div class="opciones-vista">
+        <div class="opcion-recuadro" onclick="mostrarReporte('13gas_be')">13 GAS BE</div>
+        <div class="opcion-recuadro" onclick="mostrarReporte('wigas_be')">WIGAS BE</div>
+        <div class="opcion-recuadro" onclick="mostrarReporte('13gas_vd')">13 GAS Ventas</div>
+        <div class="opcion-recuadro" onclick="mostrarReporte('wigas_vd')">WIGAS Ventas</div>
+        <div class="opcion-recuadro" onclick="mostrarReporte('13gas_tool')">13 GAS Tool</div>
+        <div class="opcion-recuadro" onclick="mostrarReporte('wigas_tool')">WIGAS Tool</div>
+        <div class="opcion-recuadro" onclick="mostrarReporte('json_tool')">Cumplimiento JSON</div>
+      </div>
+    </div>
+  `;
+  document.getElementById('contenedor').innerHTML = contenido;
+}
+
+// ====== Mostrar acuses ======
+async function mostrarAcuses(estacion) {
+  const response = await fetch('assets/pdf/acuses/metadata.json');
+  const metadata = await response.json();
+  const ejercicios = Object.keys(metadata[estacion]);
+
+  let contenido = renderBanner();
+  contenido += `
+    <div class="card p-3 bienvenida banner-view">
+      <h2>Acuses ${estacion}</h2>
+      <p>Seleccione el año:</p>
+      <div class="opciones-vista">`;
+
+  ejercicios.forEach(ejercicio => {
+    contenido += `
+      <div class="card opcion-recuadro" onclick="mostrarAcusesPorEjercicio('${estacion}', '${ejercicio}')">
+        <h4 class="texto">${ejercicio}</h4>
+      </div>`;
+  });
+
+  contenido += `</div></div>`;
+  document.getElementById('contenedor').innerHTML = contenido;
+}
+
+// ====== Mostrar acuses por ejercicio ======
 async function mostrarAcusesPorEjercicio(estacion, ejercicio) {
   const response = await fetch('assets/pdf/acuses/metadata.json');
   const metadata = await response.json();
@@ -152,8 +165,7 @@ async function mostrarAcusesPorEjercicio(estacion, ejercicio) {
   contenido += `
     <div class="card p-3 bienvenida banner-view">
       <h2>Acuses ${estacion} - ${ejercicio}</h2>
-      <p>Seleccione el mes:</p>
-      <div class="opciones-vista">`;
+      <p>Seleccione el mes:</p>`;
 
   for (const mes in meses) {
     contenido += `
@@ -171,79 +183,26 @@ async function mostrarAcusesPorEjercicio(estacion, ejercicio) {
     contenido += `</div></div>`;
   }
 
-  contenido += `</div></div>`;
+  contenido += `</div>`;
   document.getElementById('contenedor').innerHTML = contenido;
 }
 
-async function mostrarAcusesPorEjercicio(estacion, ejercicio) {
-  const response = await fetch('assets/pdf/acuses/metadata.json');
-  const metadata = await response.json();
-  const meses = metadata[estacion][ejercicio];
-
-  let contenido = renderBanner();
-  contenido += `
-    <div class="card p-3 bienvenida banner-view">
-      <h2>Acuses ${estacion} - ${ejercicio}</h2>
-      <p>Seleccione el mes:</p>
-      <div class="opciones-vista">`;
-
-  for (const mes in meses) {
-    contenido += `
-      <div class="opcion-recuadro">
-        <span class="texto">${mes}</span>
-        <div class="opciones-vista">`;
-
-    meses[mes].forEach(pdf => {
-      contenido += `
-        <div class="opcion-recuadro">
-          <a href="assets/pdf/acuses/${estacion}/${ejercicio}/${pdf}" target="_blank" class="texto">${pdf}</a>
-        </div>`;
-    });
-
-    contenido += `</div></div>`;
-  }
-
-  contenido += `</div></div>`;
-  document.getElementById('contenedor').innerHTML = contenido;
-}
-
-// Reportes Power BI (clase banner-view)
+// ====== Reportes Power BI ======
 function mostrarReporte(estacion) {
   let src = '';
 
-  // Balance de Energía
-  if(estacion === '13gas_be') {
-    src = "https://app.powerbi.com/reportEmbed?reportId=56676f9f-5b2a-48bb-9e70-01bcba832eb2&autoAuth=true&ctid=fed0588c-2eb2-4466-bb01-afd3795657ec";
-  } else if(estacion === 'bexica_be') {
-    src = "https://app.powerbi.com/reportEmbed?reportId=bd69620e-cb6f-4401-b25d-bd3c401c909c&autoAuth=true&ctid=fed0588c-2eb2-4466-bb01-afd3795657ec";
-  } else if(estacion === 'coacalco_be') {
-    src = "https://app.powerbi.com/reportEmbed?reportId=e3694e0a-353c-4957-98bc-5a7055e72f93&autoAuth=true&ctid=fed0588c-2eb2-4466-bb01-afd3795657ec";
-  } else if(estacion === 'ecatepec_be') {
-    src = "https://app.powerbi.com/reportEmbed?reportId=a011b09e-67fa-4c91-99bb-64563a0b35bf&autoAuth=true&ctid=fed0588c-2eb2-4466-bb01-afd3795657ec";
-  } else if(estacion === 'tlanepantla_be') {
-    src = "https://app.powerbi.com/reportEmbed?reportId=a011b09e-67fa-4c91-99bb-64563a0b35bf&autoAuth=true&ctid=fed0588c-2eb2-4466-bb01-afd3795657ec";
-  } else if(estacion === 'landsegen_be') {
-    src = "https://app.powerbi.com/reportEmbed?reportId=802df296-a9ee-42ed-b59c-00d0c5393b22&autoAuth=true&ctid=fed0588c-2eb2-4466-bb01-afd3795657ec";
-  } else if(estacion === 'wigas_be') {
+  if(estacion === 'wigas_be') {
     src = "https://app.powerbi.com/reportEmbed?reportId=40572fd5-d90b-441b-acac-30eae98e545b&autoAuth=true&ctid=fed0588c-2eb2-4466-bb01-afd3795657ec";
-
-  // Ventas
+  } else if(estacion === '13gas_be') {
+    src = "https://app.powerbi.com/reportEmbed?reportId=56676f9f-5b2a-48bb-9e70-01bcba832eb2&autoAuth=true&ctid=fed0588c-2eb2-4466-bb01-afd3795657ec";
   } else if(estacion === '13gas_vd') {
     src = "https://app.powerbi.com/reportEmbed?reportId=3a0bd50d-1c22-47a5-93e6-64724f740208&autoAuth=true&ctid=fed0588c-2eb2-4466-bb01-afd3795657ec";
-  } else if(estacion === 'landsegen_vd') {
-    src = "https://app.powerbi.com/reportEmbed?reportId=8dfc39db-c41e-4e4c-b36c-4fbff34edf06&autoAuth=true&ctid=fed0588c-2eb2-4466-bb01-afd3795657ec";
   } else if(estacion === 'wigas_vd') {
     src = "https://app.powerbi.com/reportEmbed?reportId=094fff5e-dbe2-459c-9c5a-b189dcac71e9&autoAuth=true&ctid=fed0588c-2eb2-4466-bb01-afd3795657ec";
-  } else if(estacion === 'ventxcliente_trimes') {
-    src = "https://app.powerbi.com/reportEmbed?reportId=2b5357da-f62c-4575-8499-e7e666291b3d&autoAuth=true&ctid=fed0588c-2eb2-4466-bb01-afd3795657ec";
-
-  // Herramientas
   } else if(estacion === '13gas_tool') {
     src = "https://app.powerbi.com/reportEmbed?reportId=318743fa-05d5-40a5-b0db-11ae78c595e5&autoAuth=true&ctid=fed0588c-2eb2-4466-bb01-afd3795657ec";
   } else if(estacion === 'wigas_tool') {
     src = "https://app.powerbi.com/reportEmbed?reportId=02fe3d46-7d4e-41a2-84a5-f863b364643a&autoAuth=true&ctid=fed0588c-2eb2-4466-bb01-afd3795657ec";
-
-  // Cumplimiento
   } else if(estacion === 'json_tool') {
     src = "https://app.powerbi.com/reportEmbed?reportId=71c98777-7533-4821-b145-4ad8c9cc2e9d&autoAuth=true&ctid=fed0588c-2eb2-4466-bb01-afd3795657ec";
   }
@@ -255,7 +214,8 @@ function mostrarReporte(estacion) {
     '</div>';
 }
 
-// Inicializar la vista
+// ====== Inicializar ======
 document.addEventListener('DOMContentLoaded', mostrarBienvenida);
 
-console.log("Portal listo: bienvenida con clase inicio-view, categorías y reportes con clase banner-view");
+console.log("Portal listo: bienvenida con clase inicio-view, categorías y reportes con clase banner-view, acuses integrados desde metadata.json");
+
